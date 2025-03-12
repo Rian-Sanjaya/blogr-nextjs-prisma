08dbcc328ce64533cf5332f442381fbcc2a83072
Rian Sanjaya <rian.sj@gmail.com>
2025-03-12 20:49:52 +0700

    add new post functionality

---
+++ pages/create.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import Router from "next/router";
+
+const Draft: React.FC = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const submitData = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+  };
+
+  return (
+    <Layout>
+      <div>
+
+      </div>
+    </Layout>
+  );
+};
+
+export default Draft;
